2a922ad8f73fe1ee2ab3e40e4f52f8c10439b4e8
Regelity <mszx2000@gmail.com>
2025-04-03 00:25:23 +0300

    fix redis url

--- apps/gateway/src/redis/redis.module.ts
+++ apps/gateway/src/redis/redis.module.ts
@@ -9,14 +9,19 @@ import { REDIS } from './redis.constants';
     {
       provide: REDIS,
       useFactory: async (configService: ConfigService) => {
-        const redisUrl = process.env.NODE_ENV === 'production' ? configService.get<string>('REDIS_URL') : "redis://localhost:6379";
-        const client = Redis.createClient({ url: redisUrl, legacyMode: true  });
-        await client.connect()
-        return client
+        const redisUrl = process.env.NODE_ENV === 'production'
+          ? configService.get<string>('REDIS_URL')
+          : "redis://localhost:6379";
+
+        console.log('Using Redis URL:', redisUrl);
+
+        const client = Redis.createClient({ url: redisUrl, legacyMode: true });
+        await client.connect();
+        return client;
       },
       inject: [ConfigService],
     },
   ],
   exports: [REDIS],
 })
-export class RedisModule {}
+export class RedisModule { }
